f070012a209e18f6c081c3eeae737285747edd7e
aehrlich1 <anatol.ehrlich@gmail.com>
2022-10-21 17:00:05 +0200

    ridge regression and refactoring

--- exercise_01/exercise_01.py
+++ exercise_01/exercise_01.py
@@ -1,36 +1,25 @@
 """
-This module does blah blah.
-
+This script solves the exercises from exercise_01
+of the class Applied Machine Learning (AML)
 """
 
 import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
+from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.model_selection import GridSearchCV
 
 # Define OS independent paths
 working_dir_path = os.path.dirname(__file__)
 car_price_path = os.path.join(working_dir_path, "ressources\\car_price.csv")
 
-
 # Create the data frames
-df = pd.read_csv(car_price_path, index_col="car_ID")
-data = df[['curbweight', 'enginesize', 'highwaympg',
-           'horsepower', 'citympg', 'peakrpm', 'price']]
-
-
-# Perform preliminary analysis
-scatter_matrix = pd.plotting.scatter_matrix(data, figsize=(10, 10))
-plt.savefig(fname=working_dir_path + '/scatter_matrix.pdf')
-corr_matrix = pd.DataFrame.corr(data)
-print('Correlation Matrix:')
-print(corr_matrix, end='\n\n')
-
+data = pd.read_csv(car_price_path, index_col="car_ID")
+data = data[['curbweight', 'enginesize', 'highwaympg',
+             'horsepower', 'citympg', 'peakrpm', 'price']]
 
 # Split data into training and test data
 X = data[['curbweight', 'enginesize', 'highwaympg',
@@ -39,25 +28,98 @@ y = data.price
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
 
+def preliminary_analysis():
+    """"
+    Documentation of function
+    """
+    # Perform preliminary analysis
+    scatter_matrix = pd.plotting.scatter_matrix(data, figsize=(10, 10))
+    plt.savefig(fname=working_dir_path + '/scatter_matrix.pdf')
+    corr_matrix = pd.DataFrame.corr(data)
+    print('Correlation Matrix:')
+    print(corr_matrix, end='\n\n')
+
+
 # 2a. Linear Regression
-# Set up the object and train the model
-linear_regression = LinearRegression().fit(X_train, y_train)
+def linear_regression():
+    """"
+    Documentation of function
+    """
+    print('========== 2a. Linear Regression ==========')
 
-# Validate the model
-cvs = cross_val_score(linear_regression, X_test, y_test)
-print(f'Cross validation score: {cvs}')
+    # Set up the object and train the model
+    lin_regression = LinearRegression().fit(X_train, y_train)
 
-# Calculate the mean score
-mean = np.mean(cvs)
-print(f'Mean cross validation score: {mean:.3f}')
+    # Validate the model
+    cvs = cross_val_score(lin_regression, X_train, y_train)
+    print(f'Cross validation score: {cvs}')
+
+    # Calculate the mean score
+    mean = np.mean(cvs)
+    print(f'Mean cross validation score: {mean:.3f}', end='\n\n')
 
 
 # 2b. Polynomial Regression
-poly_reg = Pipeline([
-    ("poly_features", PolynomialFeatures()),
-    ("lin_reg", LinearRegression())
-])
+def polynomial_regression():
+    """"
+    Documentation of function
+    """
+    print('========== 2b. Polynomial Regression ==========')
 
-# cross validation
-grid_search = GridSearchCV(poly_reg, {"poly_features__degree": [1, 2, 3]})
-grid_search.fit(X_train, y_train)
+    # create the pipeline
+    pipeline = Pipeline([
+        ("poly_features", PolynomialFeatures()),
+        ("lin_reg", LinearRegression())
+    ])
+
+    # perform cross validation
+    grid_search = GridSearchCV(
+        pipeline, {"poly_features__degree": [1, 2, 3, 4, 5]})
+    grid_search.fit(X_train, y_train)
+
+    # print best hyperparamter alpha for polynomial regrssion
+    print(f'Best paramters found: {grid_search.best_params_}')
+
+    # validate the model
+    cvs = cross_val_score(grid_search, X_train, y_train)
+    print(f'Cross validation score: {cvs}')
+
+    # Calculate the mean score
+    mean = np.mean(cvs)
+    print(f'Mean cross validation score: {mean:.3f}', end='\n\n')
+
+
+# 2c. Ridge Regression
+def ridge_regression():
+    """"
+    Documentation of function
+    """
+    print('========== 2c. Ridge Regression ==========')
+
+    # create the pipeline
+    pipeline = Pipeline([
+        ("poly_features", PolynomialFeatures()),
+        ("ridge_reg", Ridge())
+    ])
+
+    grid_search = GridSearchCV(
+        pipeline, {"poly_features__degree": [1],
+                   "ridge_reg__alpha": [0, 1, 2, 5, 10, 100, 1e3, 1e4, 1e5]})
+    grid_search.fit(X_train, y_train)
+
+    # print best hyperparamter alpha for ridge regression
+    print(f'Best paramters found: {grid_search.best_params_}')
+
+    # perform cross validation
+    cvs = cross_val_score(grid_search, X_train, y_train)
+    print(f'Cross validation score: {cvs}')
+
+    # Calculate the mean score
+    mean = np.mean(cvs)
+    print(f'Mean cross validation score: {mean:.3f}', end='\n\n')
+
+
+# Run Tasks
+linear_regression()
+polynomial_regression()
+ridge_regression()
